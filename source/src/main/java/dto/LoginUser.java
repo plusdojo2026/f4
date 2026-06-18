634e2712565b0af360c02cff013a27a8cec5052e
plusdojo2026-okamoto <okamoto-reiji-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 11:28:56 +0900

    Update LoginUser.java

--- source/src/main/java/dto/LoginUser.java
+++ source/src/main/java/dto/LoginUser.java
@@ -1,5 +1,23 @@
 package dto;
 
-public class LoginUser {
+import java.io.Serializable;
 
+public class LoginUser implements Serializable {
+	private String user_name; // ログイン時のID
+
+	public String getId() {
+		return user_name;
+	}
+
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public LoginUser() {
+		this(null);
+	}
+
+	public LoginUser(String user_name) {
+		this.user_name = user_name;
+	}
 }
