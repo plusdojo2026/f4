49a4b2123269d5a2e606b8d4710789d1fe781c25
plusdojo2026-ikarashi <ikarashi-mairo-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 13:47:18 +0900

    Update Daily_record.java

--- source/src/main/java/dto/Daily_record.java
+++ source/src/main/java/dto/Daily_record.java
@@ -1,5 +1,30 @@
 package dto;
 
-public class Daily_record {
+import java.io.Serializable;
 
+public class Daily_record implements Serializable {
+	private String mind;
+	private String body;
+	
+public Daily_record(String mind,String bosy) {
+	this.mind=mind;
+	this.body=body;
+}
+
+public Daily_record(){
+	this.mind="";
+	this.body="";
+}
+public String getMind() {
+	return mind;
+}
+public void setMind(String mind) {
+	this.mind = mind;
+}
+public String getBody() {
+	return body;
+}
+public void setBody(String body) {
+	this.body = body;
+}
 }
