512bc2be66fc27557bfe80b0c7e9a6238af57d48
plusdojo2026-yoshimura <yoshimura-koutarou-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 14:35:19 +0900

    Update Item.java

--- source/src/main/java/dto/Item.java
+++ source/src/main/java/dto/Item.java
@@ -1,5 +1,39 @@
 package dto;
 
-public class Item {
+import java.io.Serializable;
 
+public class Item implements Serializable{
+	private int itemId;			//アイテムID
+	private String itemName;	//アイテム名
+	private String itemImg;		//アイテム画像
+	public int getItemId() {
+		return itemId;
+	}
+	public void setItemId(int itemId) {
+		this.itemId = itemId;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public String getItemImg() {
+		return itemImg;
+	}
+	public void setItemImg(String itemImg) {
+		this.itemImg = itemImg;
+	}
+	public Item(int itemId, String itemName, String itemImg) {
+		super();
+		this.itemId = itemId;
+		this.itemName = itemName;
+		this.itemImg = itemImg;
+	}
+	public Item() {
+		super();
+		this.itemId = 0;
+		this.itemName = "";
+		this.itemImg = "";
+	}
 }
