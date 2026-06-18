15819373c6adaa63230ebb703967907be7a77454
plusdojo2026-yoshimura <yoshimura-koutarou-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 14:35:13 +0900

    Update Character.java

--- source/src/main/java/dto/Character.java
+++ source/src/main/java/dto/Character.java
@@ -1,6 +1,40 @@
 package dto;
 
-public class Character {
-	
+import java.io.Serializable;
+
+public class Character implements Serializable {
+	private int characterId;		 //キャラクターID
+	private String characterName;   //キャラクター名
+	private String characterMainImg;//キャラクター画像  
+	public int getCharacterId() {
+		return characterId;
+	}
+	public void setCharacterId(int characterId) {
+		this.characterId = characterId;
+	}
+	public String getCharacterName() {
+		return characterName;
+	}
+	public void setCharacterName(String characterName) {
+		this.characterName = characterName;
+	}
+	public String getCharacterMainImg() {
+		return characterMainImg;
+	}
+	public void setCharacterMainImg(String characterMainImg) {
+		this.characterMainImg = characterMainImg;
+	}
+	public Character(int characterId, String characterName, String characterMainImg) {
+		super();
+		this.characterId = characterId;
+		this.characterName = characterName;
+		this.characterMainImg = characterMainImg;
+	}
+	public Character() {
+		this.characterId = 0;
+		this.characterName = "";
+		this.characterMainImg = "";
+	}
+	 
 
 }
