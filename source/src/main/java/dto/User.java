cfa451489faa876d8c17f56f59a97fbd57c5c6f9
plusdojo2026-okamoto <okamoto-reiji-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 11:52:10 +0900

    Update User.java

--- source/src/main/java/dto/User.java
+++ source/src/main/java/dto/User.java
@@ -3,33 +3,46 @@ package dto;
 import java.io.Serializable;
 
 public class User implements Serializable {
-	private String user_name; // ID
-	private String password; // パスワード
-
+	private int userId;		 // 自動採番
+	private String userName; // ユーザー名（入力）
+	private String password; // パスワード（入力）
+	private String qrCode;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
 	public String getUserName() {
-		return user_name;
+		return userName;
 	}
-
-	public void setUserName(String user_name) {
-		this.user_name = user_name;
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-	public User(String user_name, String password) {
-		this.user_name = user_name;
+	public String getQrCode() {
+		return qrCode;
+	}
+	public void setQrCode(String qrCode) {
+		this.qrCode = qrCode;
+	}
+	public User(int userId, String userName, String password, String qrCode) {
+		this.userId = userId;
+		this.userName = userName;
 		this.password = password;
+		this.qrCode = qrCode;
+	}
+	public User() {
+		this.userId = 0;
+		this.userName = "";
+		this.password = "";
+		this.qrCode = "";
 	}
 
-	public User() {
-		this.user_name = "";
-		this.password = "";
-	}
 
 }
