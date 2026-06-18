3139c79a19f948aa55a9d6fdb148d6cd04e546f6
plusdojo2026-F-yamaguchi <yamaguchi-yuki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 13:41:46 +0900

    Update Character.java

--- source/src/main/java/dto/Character.java
+++ source/src/main/java/dto/Character.java
@@ -2,22 +2,5 @@ package dto;
 
 public class Character {
 	
-	private String pettan;
-	private String guttari;
-	private String hikikomori;
-	private String usuusu;
-	private String moyamoya;
-	private String fuwafuwa;
-	private String maamaa;
-	private String iikamo;
-	private String ukiuki;
-	private String runnrunn;
-	private String ;
-	private String;
-	private String;
-	private String;
-	private String;
-	private String;
-	private String;
 
 }
