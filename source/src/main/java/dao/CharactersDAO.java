f488b0be824cf654625a55f9cdc0a0e25c9dcb6f
plusdojo2026-yoshimura <yoshimura-koutarou-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 14:28:22 +0900

    Update CharactersDAO.java

--- source/src/main/java/dao/CharactersDAO.java
+++ source/src/main/java/dao/CharactersDAO.java
@@ -2,6 +2,8 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +11,7 @@ import java.util.List;
 import dto.Character;
 
 public class CharactersDAO {
-	public List<Character> select(Character character){
+	public List<Character> select(int characterId){
 		List<Character> characterList = new ArrayList<Character>();
 		Connection conn = null;
 		
@@ -23,12 +25,28 @@ public class CharactersDAO {
 					"root", "password");
 			
 			// SQL文を準備する
+			String sql = "SELECT * "
+					+ "FROM characters "
+					+ "WHERE character_id = ?";
+			
+			PreparedStatement pStmt = conn.prepareStatement(sql);
 			
 			// SQL文を完成させる
+			pStmt.setInt(1, characterId);
+			
 			
 			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
 			
 			// 結果表をコレクションにコピーする
+			if(rs.next()) {
+				Character c = new Character(
+						rs.getInt("character_id"),
+						rs.getString("character_name"),
+						rs.getString("character_main_img")
+						);
+				characterList.add(c);
+			}
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
