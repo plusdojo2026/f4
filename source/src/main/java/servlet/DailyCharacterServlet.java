8ce457e7b1191c6fc8deb9d0d5d5924af1a8ca22
plusdojo2026-yoshimura <yoshimura-koutarou-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 14:28:25 +0900

    Update DailyCharacterServlet.java

--- source/src/main/java/servlet/DailyCharacterServlet.java
+++ source/src/main/java/servlet/DailyCharacterServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.CharactersDAO;
+import dto.Character;
 /**
  * Servlet implementation class DailyCharacterServlet
  */
@@ -38,7 +41,56 @@ public class DailyCharacterServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//doGet(request, response);
-				
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		//リクエストパラメータを取得する
+		int karada = Integer.parseInt(request.getParameter("karada"));
+		int kibun = Integer.parseInt(request.getParameter("kibun"));
+		
+		int characterId = 0;
+		
+		if(karada >= 70 && karada <= 100 && kibun >= 0 && kibun <= 35) {
+			characterId = 1;
+		}else if(karada >= 0 && karada <= 20 && kibun >= 30 && kibun <= 60) {
+			characterId = 2;
+		}else if(karada >= 0 && karada <= 20 && kibun >= 15 && kibun <= 30) {
+			characterId = 3;
+		}else if(karada >= 40 && karada <= 60 && kibun >= 40 && kibun <= 60) {
+			characterId = 7;
+		}else if(karada >= 65 && karada <= 100 && kibun >= 35 && kibun <= 65) {
+			characterId = 8;
+		}else if(karada >= 40 && karada <= 70 && kibun >= 85 && kibun <= 100) {
+			characterId = 9;
+		}else if(karada >= 90 && karada <= 100 && kibun >= 90 && kibun <= 100) {
+			characterId = 10;
+		}else if(karada >= 70 && karada <= 90 && kibun >= 70 && kibun <= 100) {
+			characterId = 11;
+		}else if(karada >= 0 && karada <= 40 && kibun >= 85 && kibun <= 100) {
+			characterId = 12;
+		}else if(karada >= 40 && karada <= 60 && kibun >= 15 && kibun <= 30) {
+			characterId = 13;
+		}else if(karada >= 20 && karada <= 35 && kibun >= 20 && kibun <= 40) {
+			characterId = 14;
+		}else if(karada >= 60 && karada <= 70 && kibun >= 10 && kibun <= 30) {
+			characterId = 15;
+		}else if(karada >= 0 && karada <= 10 && kibun >= 20 && kibun <= 70) {
+			characterId = 16;
+		}else if(karada >= 0 && karada <= 10 && kibun >= 0 && kibun <= 10) {
+			characterId = 17;
+		}else {
+			characterId = 4;
+			//characterId = 5;
+			//characterId = 6;
+		}
+		
+		
+		CharactersDAO charaDao= new CharactersDAO();
+		List<Character> characterList = charaDao.select(characterId);
+		
+		// 検索結果をリクエストスコープに格納する
+		request.setAttribute("characterList", characterList);
+		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/home.jsp");
 		dispatcher.forward(request, response);
 	}
