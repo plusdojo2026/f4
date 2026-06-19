fb33034ff7fee7ec6d347921f933594a395ecdf7
plusdojo2026-okamoto <okamoto-reiji-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-19 11:24:23 +0900

    Update LoginServlet.java

--- source/src/main/java/servlet/LoginServlet.java
+++ source/src/main/java/servlet/LoginServlet.java
@@ -45,7 +45,7 @@ public class LoginServlet extends HttpServlet {
 
 		// ログイン処理を行う
 		UsersDAO iDao = new UsersDAO();
-		if (iDao.isLoginOK(new User(userName, password))) { // ログイン成功
+		if (iDao.isLoginOK(new User(0,userName, password,""))) { // ログイン成功
 			// セッションスコープにIDを格納する
 			HttpSession session = request.getSession();
 			session.setAttribute("id", new LoginUser(userName));
