3d954b4bc1018e016f4b4871fa43caa2d0a4d378
plusdojo2026-okamoto <okamoto-reiji-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-22 11:27:47 +0900

    Update RegistServlet.java

--- source/src/main/java/servlet/RegistServlet.java
+++ source/src/main/java/servlet/RegistServlet.java
@@ -51,24 +51,15 @@ public class RegistServlet extends HttpServlet {
 
 		// リクエストパラメータを取得する
 		request.setCharacterEncoding("UTF-8");
-		String company = request.getParameter("company");
-		String department = request.getParameter("department");
-		String position = request.getParameter("position");
-		String name = request.getParameter("name");
-		String zipcode = request.getParameter("zipcode");
-		String address = request.getParameter("address");
-		String phone = request.getParameter("phone");
-		String fax = request.getParameter("fax");
-		String email = request.getParameter("email");
-		String remarks = request.getParameter("remarks");
+		String company = request.getParameter("userId");
+		String department = request.getParameter("password");
 
 		// 登録処理を行う
-		BcDAO bDao = new BcDAO();
-		if (bDao.insert(new Bc(0, company,  department, position, name, zipcode, address, 
-				phone, fax, email, remarks))) { // 登録成功
-			request.setAttribute("result", new Result("登録成功！", "レコードを登録しました。", "/webappAns/MenuServlet"));
+		UsersDAO uDao = new UsersDAO();
+		if (uDao.insert(new User(0, company, ""))) { // 登録成功
+			request.setAttribute("result", new Result("新規登録成功！", "アカウントを登録しました。", "/f4/LoginServlet"));
 		} else { // 登録失敗
-			request.setAttribute("result", new Result("登録失敗！", "レコードを登録できませんでした。", "/webappAns/MenuServlet"));
+			request.setAttribute("result", new Result("新規登録失敗！", "アカウントを登録できませんでした。", "/f4/ResistServlet"));
 		}
 
 		// 結果ページにフォワードする
