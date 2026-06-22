809f6f268585098bec6f575e2882488318009d1f
plusdojo2026-okamoto <okamoto-reiji-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-22 11:13:14 +0900

    Update RegistServlet.java

--- source/src/main/java/servlet/RegistServlet.java
+++ source/src/main/java/servlet/RegistServlet.java
@@ -8,6 +8,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.BcDAO;
+import dto.Bc;
+import dto.Result;
 
 /**
  * Servlet implementation class RegistServlet
@@ -42,11 +47,33 @@ public class RegistServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//doGet(request, response);
-		
-		        // ホーム画面にフォワードする
-				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/regist.jsp");
-				dispatcher.forward(request, response);
-				
+		HttpSession session = request.getSession();
+
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String company = request.getParameter("company");
+		String department = request.getParameter("department");
+		String position = request.getParameter("position");
+		String name = request.getParameter("name");
+		String zipcode = request.getParameter("zipcode");
+		String address = request.getParameter("address");
+		String phone = request.getParameter("phone");
+		String fax = request.getParameter("fax");
+		String email = request.getParameter("email");
+		String remarks = request.getParameter("remarks");
+
+		// 登録処理を行う
+		BcDAO bDao = new BcDAO();
+		if (bDao.insert(new Bc(0, company,  department, position, name, zipcode, address, 
+				phone, fax, email, remarks))) { // 登録成功
+			request.setAttribute("result", new Result("登録成功！", "レコードを登録しました。", "/webappAns/MenuServlet"));
+		} else { // 登録失敗
+			request.setAttribute("result", new Result("登録失敗！", "レコードを登録できませんでした。", "/webappAns/MenuServlet"));
+		}
+
+		// 結果ページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+		dispatcher.forward(request, response);
+	}
 	}
 }
