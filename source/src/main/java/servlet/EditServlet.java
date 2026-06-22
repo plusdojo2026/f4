b2f1e10af72d4f288b30d8b6574ed61e50251e98
plusdojo2026-F-yamaguchi <yamaguchi-yuki-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-22 14:41:09 +0900

    Update EditServlet.java

--- source/src/main/java/servlet/EditServlet.java
+++ source/src/main/java/servlet/EditServlet.java
@@ -1,6 +1,10 @@
 package servlet;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -53,6 +57,19 @@ public class EditServlet extends HttpServlet {
             request.setCharacterEncoding("UTF-8");
 
             String imageData = request.getParameter("formData");
+            
+            String base64 = imageData.split(",")[1];
+
+            byte[] imageBytes = Base64.getDecoder().decode(base64);
+            
+            //保存先を取得
+            String path = getServletContext().getRealPath("/images");
+            //ファイル名
+            String fileName = "user1.png";
+            //
+            Path filePath = Paths.get(path, fileName);
+
+            Files.write(filePath, imageBytes);
 
             //HttpSession session = request.getSession();
 
@@ -66,7 +83,7 @@ public class EditServlet extends HttpServlet {
 
             //dto.setUserId(userId);
             //dto.setSaveDate(saveDate);
-            dto.setEditScreenShot(imageData);
+            dto.setEditScreenShot(fileName);
             
             EditDAO dao = new EditDAO();
             dao.update(dto);
