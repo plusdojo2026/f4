40e8007c209e5c320b0cce31149b41e37f82d356
plusdojo2026-okamoto <okamoto-reiji-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-24 16:18:56 +0900

    Update DailyCharacterServlet.java

--- source/src/main/java/servlet/DailyCharacterServlet.java
+++ source/src/main/java/servlet/DailyCharacterServlet.java
@@ -2,7 +2,6 @@ package servlet;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Random;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,8 +13,11 @@ import javax.servlet.http.HttpSession;
 
 import dao.Character_subsDAO;
 import dao.CharactersDAO;
+import dao.Daily_recordsDAO;
 import dto.Character;
-import dto.Character_sub; 
+import dto.Character_sub;
+import dto.Daily_record;
+import dto.LoginUser; 
 /**
  * Servlet implementation class DailyCharacterServlet
  */
@@ -37,6 +39,38 @@ public class DailyCharacterServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setCharacterEncoding("UTF-8");
+		
+		HttpSession session = request.getSession();
+		LoginUser user = (LoginUser) session.getAttribute("id");
+		
+		// もしユーザーID取得なし、つまり、うまくログインできていないなら、ログイン画面へ帰ってもらう。
+		if (user == null) {
+			response.sendRedirect(request.getContextPath() + "/LoginServlet");
+			return;
+		}
+		
+		// userId取得（QRに組み込むため）
+		String userId = user.getUserId();
+		
+		// characterId検索
+		Daily_recordsDAO dao = new Daily_recordsDAO();
+		Daily_record record = dao.select(userId);
+		
+		int characterId = record.getCharacterId();
+		System.out.println(characterId);
+		
+		CharactersDAO charaDao= new CharactersDAO();
+		List<Character> characterList = charaDao.select(characterId);
+		
+		// 検索結果をリクエストスコープに格納する
+		//request.setAttribute("characterList", characterList);
+		session.setAttribute("characterList", characterList);
+		
+		Character_subsDAO subDao = new Character_subsDAO();
+		List<Character_sub> characterSubList = subDao.select(characterId);
+		
+		request.setAttribute("characterSubList",characterSubList);
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/home.jsp");
 		dispatcher.forward(request, response);
@@ -51,53 +85,30 @@ public class DailyCharacterServlet extends HttpServlet {
 		
 		request.setCharacterEncoding("UTF-8");
 		
-		//リクエストパラメータを取得する
-		int body = Integer.parseInt(request.getParameter("body"));
-		int mind = Integer.parseInt(request.getParameter("mind"));
+		HttpSession session = request.getSession();
+		LoginUser user = (LoginUser) session.getAttribute("id");
 		
-		int characterId = 0;
-		Random rand = new Random();
-		
-		if(body >= 70 && body <= 100 && mind >= 0 && mind <= 35) {
-			characterId = 1;
-		}else if(body >= 0 && body <= 20 && mind >= 30 && mind <= 60) {
-			characterId = 2;
-		}else if(body >= 0 && body <= 20 && mind >= 15 && mind <= 30) {
-			characterId = 3;
-		}else if(body >= 40 && body <= 60 && mind >= 40 && mind <= 60) {
-			characterId = 7;
-		}else if(body >= 65 && body <= 100 && mind >= 35 && mind <= 65) {
-			characterId = 8;
-		}else if(body >= 40 && body <= 70 && mind >= 85 && mind <= 100) {
-			characterId = 9;
-		}else if(body >= 90 && body <= 100 && mind >= 90 && mind <= 100) {
-			characterId = 10;
-		}else if(body >= 70 && body <= 90 && mind >= 70 && mind <= 100) {
-			characterId = 11;
-		}else if(body >= 0 && body <= 40 && mind >= 85 && mind <= 100) {
-			characterId = 12;
-		}else if(body >= 40 && body <= 60 && mind >= 15 && mind <= 30) {
-			characterId = 13;
-		}else if(body >= 20 && body <= 35 && mind >= 20 && mind <= 40) {
-			characterId = 14;
-		}else if(body >= 60 && body <= 70 && mind >= 10 && mind <= 30) {
-			characterId = 15;
-		}else if(body >= 0 && body <= 10 && mind >= 20 && mind <= 70) {
-			characterId = 16;
-		}else if(body >= 0 && body <= 10 && mind >= 0 && mind <= 10) {
-			characterId = 17;
-		}else {
-			characterId = rand.nextInt(3) + 4;
-			
+		// もしユーザーID取得なし、つまり、うまくログインできていないなら、ログイン画面へ帰ってもらう。
+		if (user == null) {
+			response.sendRedirect(request.getContextPath() + "/LoginServlet");
+			return;
 		}
 		
+		// userId取得（QRに組み込むため）
+		String userId = user.getUserId();
+		
+		// characterId検索
+		Daily_recordsDAO dao = new Daily_recordsDAO();
+		Daily_record record = dao.select(userId);
+		
+		int characterId = record.getCharacterId();
+		System.out.println(characterId);
 		
 		CharactersDAO charaDao= new CharactersDAO();
 		List<Character> characterList = charaDao.select(characterId);
 		
 		// 検索結果をリクエストスコープに格納する
 		//request.setAttribute("characterList", characterList);
-		HttpSession session = request.getSession();
 		session.setAttribute("characterList", characterList);
 		
 		Character_subsDAO subDao = new Character_subsDAO();
