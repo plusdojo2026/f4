9fa3ccd5b1abd75926f39c8876dc86c72adc8148
plusdojo2026-yoshimura <yoshimura-koutarou-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-26 11:47:39 +0900

    Update SearchDAO.java

--- source/src/main/java/dao/SearchDAO.java
+++ source/src/main/java/dao/SearchDAO.java
@@ -21,8 +21,8 @@ public class SearchDAO {
 
 			// データベースに接続する
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/f4?"
-					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-					"root", "password");
+					+ "characterEncoding=utf8&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+					"f4", "WmWxqN4QXsS7srCL");
 
 			// SQL文を準備する
 			String sql;
