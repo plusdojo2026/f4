fa87982b97f8a963eaf812c9169b1476473c34d0
plusdojo2026-yoshimura <yoshimura-koutarou-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-26 14:04:45 +0900

    Update Daily_recordsDAO.java

--- source/src/main/java/dao/Daily_recordsDAO.java
+++ source/src/main/java/dao/Daily_recordsDAO.java
@@ -112,8 +112,8 @@ public class Daily_recordsDAO {
 
 			// データベースに接続する
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/f4?"
-					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
-					"root", "password");
+					+ "characterEncoding=utf8&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+					"f4", "WmWxqN4QXsS7srCL");
 
 			// SQL文を準備する
 			String sql = "DELETE FROM daily_records WHERE user_id = ? AND create_date = CURRENT_DATE";
